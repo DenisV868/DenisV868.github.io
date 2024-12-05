c759df6e406b2496b8529cffcee20bf2b23eb987
LAPTOP_DENIS\denis <denis.vimr@gmail.com>
2024-12-05 21:56:08 +0100

    Starting E-commerce

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
+import ProductPreview from "./ProductPreview";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          React lab
+    function getUl() {
+        let lis:any = []
+        for(let i:number = 0; i < 5; i++){
+                lis.append(<ProductPreview />)
+        }
+        return <ul>
+            {lis.map((item:any) => <li>{item}</li>)}
+        </ul>
+    }
 
-        </p>
-      </header>
+    return (
+    <div className="App">
+        <header className="App-header">
+            <p>
+                React lab
+            </p>
+        </header>
+        {getUl()}
     </div>
   );
 }
---
+++ src/ProductPreview.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductPreview = () => {
+
+    return <div className={"container-for-product"}>
+        <p>Name</p>
+        <img src="https://cdn.myshoptet.com/usr/www.iphonarna.cz/user/shop/big/7035_apple-iphone-13-128gb-starlight.jpg?6387f80d" alt=""/>
+        <p>sansfknfnfnfksnfkwkndwjkfnwfknkwfnfwmnfkwfnnkd sjfvhbjfdbigvfsdhjdzfhbfdhvsfegyvwer</p>
+    </div>
+  
+}
+
+export default ProductPreview;
