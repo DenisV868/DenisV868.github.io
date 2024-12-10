76507a1f892232520606b2fa59056413aa2fab66
LAPTOP_DENIS\denis <denis.vimr@gmail.com>
2024-12-10 23:20:52 +0100

    Starting E-commerce

--- src/Product.tsx
+++ src/Product.tsx
@@ -9,14 +9,14 @@ const Product = () => {
     if (img != null) {
         const decodedImg = decodeURIComponent(img);
 
-        return<div className = "site-product">
+        return<article className = "site-product">
             <div className="product">
                 <h1>{name}</h1>
                 <img src={decodedImg} alt="" className="productImg" />
                 <p className={"desc"}>{description}</p>
-                <p className={"price"}>{price}</p>
+                <p className={"price"}>{price}</p><button><img className="addCImg" src="/add-to-cart.png" alt="" style={{width:"30px",height:"30px",position:"relative", right:"2px", bottom:"0px", top:"1px"}}/></button>
             </div>
-        </div>
+        </article>
     }
     return <h1>ERROR</h1>
 }
