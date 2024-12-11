99b04ed1e2b5fed4712377ed0232fd73bfe6417a
LAPTOP_DENIS\denis <denis.vimr@gmail.com>
2024-12-11 21:24:53 +0100

    Starting E-commerce

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import TestAuto from "./TestAuto";
-import { BrowserRouter as  Router,Routes, Route } from 'react-router-dom'; // Import necessary components
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'; // Import necessary components
 import Product from "./Product";
+
 
 function App() {
 
+
+
+    // @ts-ignore
     return (
         <Router>
             <div className="App">
@@ -21,7 +25,17 @@ function App() {
             </div>
             <footer>
                 <div>
-                    not yet finished
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+
+                        </li>
+                        <li>
+
+                        </li>
+                    </ul>
                 </div>
             </footer>
         </Router>
