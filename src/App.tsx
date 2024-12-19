3a6d03cbe8611003cab0fd71d74238cf82814af8
LAPTOP_DENIS\denis <denis.vimr@gmail.com>
2024-12-19 23:13:31 +0100

    React OS begins

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import React, {useState} from "react";
+import './App.css';
+// @ts-ignore
+import Clock from "./Clock.tsx";
+//@ts-ignore
+import Menu from "./Menu.tsx";
+
+const getDate = ():string =>{
+    let date:Date = new Date();
+    let day:number = date.getDate();
+    let month:number = date.getMonth()+1;
+    let year:number = date.getFullYear();
+    return ` | ${day}.${month}.${year}`;
+}
+
+function App() {
+
+    const [hovered, setHovered] = useState(false);
+    const [clicked, setClicked] = useState(false);
+
+    const hoverHandler = () => {
+        setHovered(true);
+    }
+
+    const mouseOf = () => {
+        if(!clicked) {
+            setHovered(false);
+        }
+    }
+
+    const clickHandler = () => {
+        if(!clicked){
+            setClicked(true);
+        }
+        if(clicked){
+            setClicked(false);
+        }
+    }
+
+    return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+
+      <footer className="panel-background">
+            <div className="panel">
+                <p className="date">
+                    {getDate()}
+                </p>
+                <Clock />
+                <p className="menu" onMouseOver={hoverHandler} onMouseOut={mouseOf} onClick={clickHandler}>
+                    <img src="/icons8-react-30.png" alt="menu" />
+                </p>
+
+                {hovered && <Menu />}
+            </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Menu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const  Menu = () =>{
+    return(<p className={"menu-menu"}></p>)
+}
+
+export default Menu;
